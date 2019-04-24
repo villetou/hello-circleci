9290c768192ff8af6ef58bf2edf6ddc7c0d898dd
Ville Touronen <ville.touronen@reaktor.com>
2019-04-24 15:59:14 +0300

    Fix dev server assets

--- razzle.config.js
+++ razzle.config.js
@@ -5,7 +5,7 @@ module.exports = {
   modify: (config, { target }) => {
     // https://github.com/jaredpalmer/razzle/issues/412#issuecomment-347760191
     // Fixes static assets path (required svgs, images, etc) for SSR
-    if (target === 'node') {
+    if (target === 'node' && process.env.NODE_ENV === 'production') {
       config.output.publicPath = `${process.env.PUBLIC_PATH}`
     }
     return config
