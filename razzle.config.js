97b6c581452aa859caa93a3720561362e0590ed7
Ville Touronen <ville.touronen@reaktor.com>
2019-04-24 14:54:54 +0300

    Add a fix for static assets public path

--- razzle.config.js
+++ razzle.config.js
@@ -2,4 +2,12 @@
 
 module.exports = {
   plugins: ['typescript'],
+  modify: (config, { target }) => {
+    // https://github.com/jaredpalmer/razzle/issues/412#issuecomment-347760191
+    // Fixes static assets path (required svgs, images, etc) for SSR
+    if (target === 'node') {
+      config.output.publicPath = `${process.env.PUBLIC_PATH}`
+    }
+    return config
+  }
 };
